48f672969826fbc534cfcf73f1c9d7ea886aaa3e
Vitor Siqueira <41929568+VitorSiqueirr@users.noreply.github.com>
2023-03-05 10:15:46 -0300

    Readers

---
+++ src/modules/readers/typeorm/entities/Readers.ts
@@ -0,0 +1,21 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+  } from "typeorm";
+  
+  @Entity("readers")
+  export default class Customer {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+    @Column()
+    name: string;
+    @Column()
+    email: string;
+    @CreateDateColumn()
+    created_at: Date;
+    @CreateDateColumn()
+    updated_at: Date;
+  }
+  
